b055420a646dca83979e9688d7d969547b8ffc02
Bradley Dice <bdice@bradleydice.com>
2017-09-01 22:29:23 -0400

    Move JS to head, fixed errors in JS event firing.

--- signac_dashboard/static/js/menu-toggle-mobile.js
+++ signac_dashboard/static/js/menu-toggle-mobile.js
@@ -1,10 +1,12 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
+  if ( $('.nav-toggle').length && $('.nav-menu').length ){
     var burger = document.querySelector('.nav-toggle');
     var menu = document.querySelector('.nav-menu');
     burger.addEventListener('click', function() {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
+  }
 })();
