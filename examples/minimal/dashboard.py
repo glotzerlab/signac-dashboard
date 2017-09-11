bc96d79b163b2a2039965e2209ac7de22f361505
Bradley Dice <bdice@bradleydice.com>
2017-09-11 19:04:40 -0400

    Updated minimal example.

--- examples/minimal/dashboard.py
+++ examples/minimal/dashboard.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 from signac_dashboard import Dashboard
-config = {
-    'PROJECT_DIR': '/path/to/signac/directory'
-}
-dashboard = Dashboard(config)
-dashboard.run(host='localhost', port=8888, debug=True)
+from signac import init_project
+
+if __name__ == '__main__':
+
+    project = init_project('dashboard-test-project')
+    config = {}
+    modules = []
+    dashboard = Dashboard(config=config, project=project, modules=modules)
+    dashboard.run(host='localhost', port=8888)
