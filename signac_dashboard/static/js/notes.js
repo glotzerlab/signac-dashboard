d4be493ab814738c5cca98d38dfbf41825e73b12
Bradley Dice <bdice@bradleydice.com>
2017-09-11 19:56:54 -0400

    Fixed notes event handling.

--- signac_dashboard/static/js/notes.js
+++ signac_dashboard/static/js/notes.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function() {
   $('form.notes-form').submit(function(event) {
     // Stop form from submitting normally
     event.preventDefault();
