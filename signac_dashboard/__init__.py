f8091020dad95e91f5b52952921f448b6a938f59
Carl Simon Adorf <csadorf@umich.edu>
2017-09-13 13:10:34 -0400

    Explicit definition of the .modules namespace.

--- signac_dashboard/__init__.py
+++ signac_dashboard/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, g
 from .dashboard import Dashboard
+from . import modules
 
 
 __version__ = '0.0.1'
 __all__ = [
-    'Dashboard'
+    'Dashboard',
+    'modules',
     ]
 
---
+++ signac_dashboard/modules/__init__.py
@@ -0,0 +1,12 @@
+from .image_viewer import ImageViewer
+from .statepoint_list import StatepointList
+from .document_list import DocumentList
+from .notes import Notes
+
+__all__ = [
+    'ImageViewer',
+    'StatepointList',
+    'DocumentList',
+    'FileList',
+    'Notes',
+    ]
