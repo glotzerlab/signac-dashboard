456ba0df12788dba42fcabb477c6cde3259a4fc3
Bradley Dice <bdice@bradleydice.com>
2017-09-17 01:01:03 -0400

    Added FileList module to modules package.

--- signac_dashboard/modules/__init__.py
+++ signac_dashboard/modules/__init__.py
@@ -1,6 +1,7 @@
 from .image_viewer import ImageViewer
 from .statepoint_list import StatepointList
 from .document_list import DocumentList
+from .file_list import FileList
 from .notes import Notes
 
 __all__ = [
