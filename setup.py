a9378288c1438cfd4fec73e97e185b1326116ab3
Bradley Dice <bdice@bradleydice.com>
2018-02-08 22:53:37 -0500

    Bump up to version 0.1.2.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 4, 0):
 
 setup(
     name='signac-dashboard',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
