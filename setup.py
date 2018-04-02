7782ba5234cde450b477c2d481a501c444b813cf
Bradley Dice <bdice@bradleydice.com>
2018-04-02 19:54:01 -0400

    Bump up to version 0.1.3.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 4, 0):
 
 setup(
     name='signac-dashboard',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
--- signac_dashboard/__init__.py
+++ signac_dashboard/__init__.py
@@ -5,7 +5,7 @@ from .dashboard import Dashboard
 from . import modules
 
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = [
     'Dashboard',
