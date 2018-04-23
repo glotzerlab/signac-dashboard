4be9008f7f52d13dd97b69206dd783cc37ab01c8
Bradley Dice <bdice@bradleydice.com>
2018-04-23 12:00:11 -0400

    Moved notes module assets out of the core static files.

--- signac_dashboard/modules/notes.py
+++ signac_dashboard/modules/notes.py
@@ -2,7 +2,8 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 from signac_dashboard.module import Module
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, abort
+from jinja2.exceptions import TemplateNotFound
 
 
 class Notes(Module):
@@ -19,17 +20,32 @@ class Notes(Module):
 
     def get_cards(self, job):
         note_text = job.document.get('notes', '')
-        notes_action = url_for('update_notes')
+        notes_action = url_for('notes_update')
         return [{'name': self.name, 'content': render_template(
             self.template, notes_action=notes_action, note_text=note_text,
             jobid=str(job))}]
 
     def register_routes(self, dashboard):
-        @dashboard.app.route('/notes/update', methods=['POST'])
-        def update_notes():
+        @dashboard.app.route('/module/notes/update', methods=['POST'])
+        def notes_update():
             note_text = request.form.get('note_text')
             jobid = request.form.get('jobid')
             job = dashboard.project.open_job(id=jobid)
             job.document['notes'] = note_text
-            # redirect(request.form.get('redirect', url_for('home')))
             return "Saved."
+
+        @dashboard.app.route('/module/notes/<path:filename>')
+        def notes_asset(filename):
+            path = 'notes/{}'.format(filename)
+            try:
+                return render_template(path)
+            except TemplateNotFound:
+                abort(404, 'The file requested does not exist.')
+
+    def register_assets(self, dashboard):
+        assets = ['js/notes.js']
+        for assetfile in assets:
+            dashboard.register_module_asset({
+                'file': 'templates/notes/{}'.format(assetfile),
+                'url': '/module/notes/{}'.format(assetfile)
+            })
