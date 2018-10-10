dbb31dcaea57e32faf1c038a9bee1f7ee51f3238
Bradley Dice <bdice@bradleydice.com>
2018-10-09 21:28:40 -0400

    Fixed doc release version.

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Bradley Dice'
 author = 'Bradley Dice'
 
 # The short X.Y version
-version = '0.1.3'
+version = '0.1.4'
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
