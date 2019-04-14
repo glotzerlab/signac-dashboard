338eaea8d89b76dd5886af738052c93069b10c34
Bradley Dice <bdice@bradleydice.com>
2019-04-14 09:04:02 -0400

    Don't require flow for import, rename project_file to project_module.

--- signac_dashboard/modules/flow_status.py
+++ signac_dashboard/modules/flow_status.py
@@ -3,11 +3,7 @@
 # This software is licensed under the BSD 3-Clause License.
 from signac_dashboard.module import Module
 from flask import render_template
-
-try:
-    from flow import FlowProject  # noqa: F401
-except ImportError:
-    raise RuntimeWarning('Need signac-flow installed.\n')
+from importlib import import_module
 
 
 class FlowStatus(Module):
@@ -16,34 +12,32 @@ class FlowStatus(Module):
     This module determines job status from a signac-flow FlowProject and shows
     the job's current labels.
 
-    :param project_file: The module containing the FlowProject (e.g.,
-        :code:`'project.py'`).
-    :type project_file: str
-    :param project_class: The name of the FlowProject class (e.g.,
-        :code:`'MyProject'`).
+    :param project_module: The Python module containing the FlowProject
+        (default: :code:`'project'`).
+    :type project_module: str
+    :param project_class: The name of the FlowProject class (default:
+        :code:`'FlowProject'`).
     :type project_class: str
     """
     def __init__(self,
                  name='Flow Project Status',
                  context='JobContext',
                  template='cards/flow_status.html',
-                 project_file=None,
-                 project_class=None,
+                 project_module='project',
+                 project_class='FlowProject',
                  **kwargs):
         super().__init__(name=name,
                          context=context,
                          template=template,
                          **kwargs)
-        self.project_file = project_file
+        self.project_module = project_module
         self.project_class = project_class
 
     def get_cards(self, job):
-        import importlib
-        if self.project_file is None:
-            return None
-        module = importlib.import_module(self.project_file)
-        my_class = getattr(module,  self.project_class)
-        obj = my_class()
-        labels = obj.labels(job)
+        if self.project_module is None or self.project_class is None:
+            return
+        module = import_module(self.project_module)
+        project = getattr(module, self.project_class)()
+        labels = project.labels(job)
         return [{'name': self.name,
                  'content': render_template(self.template, labels=labels)}]
