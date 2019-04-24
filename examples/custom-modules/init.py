50aa13d50a1f5c9842e05562972015b81e2046cb
Bradley Dice <bdice@bradleydice.com>
2019-04-24 09:14:21 -0400

    Add custom module example.

---
+++ examples/custom-modules/init.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+import signac
+
+project = signac.init_project('awesome-project')
+
+for adverb in ('very', 'super', 'really', 'extremely'):
+    job = project.open_job({'awesome_factor': adverb})
+    job.init()
