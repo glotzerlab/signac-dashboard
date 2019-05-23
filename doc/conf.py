b3ac32212c486d457dc189f0fc46d6dd58d8f4cf
Bradley Dice <bdice@bradleydice.com>
2019-05-23 09:33:10 -0400

    Bump up to version 0.2.4.

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = 'The Regents of the University of Michigan'
 author = 'Bradley D. Dice, Carl S. Adorf, Sharon C. Glotzer'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.2.4'
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
