4a30baf37f7282c688e289d1823fa1cb7320e255
Jens Glaser <glaserj@ornl.gov>
2020-04-16 19:44:24 -0400

    add an example for on-the-fly plotting

---
+++ examples/online_plots/README.md
@@ -0,0 +1,5 @@
+# Plotter Example
+
+Uses matplotlib to plot a function on-the-fly.
+
+Run with `python dashboard.py run`
---
+++ examples/online_plots/dashboard.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+from signac_dashboard import Dashboard
+from signac_dashboard.modules import StatepointList, Plotter
+
+import signac
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PlotExample(Plotter):
+    def create_figure(self,job):
+        with job:
+            import numpy
+            fig, ax = plt.subplots()
+            x = np.arange(10)
+            y = np.random.random(10)
+            ax.plot(x,y)
+            return fig
+
+
+if __name__ == '__main__':
+    modules = []
+    modules.append(StatepointList())
+    modules.append(PlotExample())
+    Dashboard(modules=modules).main()
