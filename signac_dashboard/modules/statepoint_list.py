9189786067fb7b21b3071362af596b41cbc70994
Tim Moore <tcmoore3@gmail.com>
2020-09-22 22:07:30 -0400

    Add option to exclude constant sp params in splist
    This commit adds an option hide constant statepoint parameters in the
    StatepointList module.
    
    Closes #76

--- signac_dashboard/modules/statepoint_list.py
+++ signac_dashboard/modules/statepoint_list.py
@@ -12,13 +12,26 @@ class StatepointList(Module):
                  name='Statepoint Parameters',
                  context='JobContext',
                  template='cards/statepoint_list.html',
+                 exclude_const=False,
                  **kwargs):
         super().__init__(name=name,
                          context=context,
                          template=template,
                          **kwargs)
+        self.exclude_const = exclude_const
 
     def get_cards(self, job):
-        sp = OrderedDict(sorted(job.statepoint().items(), key=lambda t: t[0]))
+        sp = [(k, v) for k, v in sorted(job.statepoint().items(),
+            key=lambda t: t[0]) if k in self.sp_keys_to_show]
+        sp = OrderedDict(sp)
         return [{'name': self.name,
                  'content': render_template(self.template, statepoint=sp)}]
+
+    def register(self, dashboard):
+        # always show all sp params for a single-job project
+        if len(dashboard.project) == 1:
+            excl_const = False
+        else:
+            excl_const = self.exclude_const
+        schema = dashboard.project.detect_schema(exclude_const=excl_const)
+        self.sp_keys_to_show = schema.keys()
