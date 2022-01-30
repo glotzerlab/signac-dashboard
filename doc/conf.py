fb9854dfa9f18106da4d2d059bd03cfe1a1b7516
Bradley Dice <bdice@bradleydice.com>
2022-01-30 12:21:12 -0600

    Add sphinx-codeautolink extension.

--- doc/conf.py
+++ doc/conf.py
@@ -45,6 +45,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx_codeautolink",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
