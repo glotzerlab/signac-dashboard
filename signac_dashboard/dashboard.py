2c49be85dc5b29484843a18f35bf565681d87ddf
Corwin Kerr <cbkerr@umich.edu>
2022-04-30 21:20:22 -0400

    Add no cards message / no modules message for project info

--- signac_dashboard/dashboard.py
+++ signac_dashboard/dashboard.py
@@ -392,6 +392,8 @@ class Dashboard:
     def _render_project_view(self, *args, **kwargs):
         g.active_page = "project"
         session["context"] = "ProjectContext"
+        if "enabled_modules" in session and len(session.get("enabled_modules", [])) == 0:
+            flash("No modules are enabled.", "info")
         return render_template("project_info.html", *args, **kwargs)
 
     def _render_error(self, error):
