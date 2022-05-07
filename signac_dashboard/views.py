0b2765ba3784911457a764264afd158a3eaac757
Bradley Dice <bdice@bradleydice.com>
2022-05-07 17:34:35 -0500

    Fix TODO.

--- signac_dashboard/views.py
+++ signac_dashboard/views.py
@@ -103,7 +103,7 @@ def get_file(dashboard, filename, jobid=None):
 def change_modules(dashboard):
     enabled_module_indices = session.get(
         "enabled_module_indices", dict(JobContext=[], ProjectContext=[])
-    )  # todo better default values
+    )  # TODO: better default values
     enabled_module_indices = {
         k: set(v) for k, v in enabled_module_indices.items()
     }  # remove duplicates
