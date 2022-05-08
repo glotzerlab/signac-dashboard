334b0af2b3f1a587b4ae98809f1125ae2b21290b
Corwin Kerr <cbkerr@umich.edu>
2022-05-07 22:00:44 -0400

    Rename schema_display file

--- signac_dashboard/modules/__init__.py
+++ signac_dashboard/modules/__init__.py
@@ -4,7 +4,7 @@ from .file_list import FileList
 from .flow_status import FlowStatus
 from .image_viewer import ImageViewer
 from .notes import Notes
-from .schema_display import Schema
+from .schema import Schema
 from .statepoint_list import StatepointList
 from .text_display import TextDisplay
 from .video_viewer import VideoViewer
