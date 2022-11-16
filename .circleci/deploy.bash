8ea5fb4900395ef2b6a9510123bcf75f9844289a
Bradley Dice <bdice@bradleydice.com>
2022-11-16 11:49:35 -0600

    Move requirements-test.txt.

--- .circleci/deploy.bash
+++ .circleci/deploy.bash
@@ -3,7 +3,7 @@
 set -e
 set -u
 
-python -m pip install --progress-bar off --user -U -r requirements-test.txt
+python -m pip install --progress-bar off --user -U -r requirements/requirements-test.txt
 python -m pip install --progress-bar off --user -U twine wheel setuptools
 
 # PYPI_API_TOKEN - (Required, Secret) Token for the publisher's account on PyPI
